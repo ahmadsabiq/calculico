ea7c2580fc98f06c7e5e8c7412d236e53f2cd1ca
ahmadsabiq <ahmadsabiqadhy@gmail.com>
2023-12-04 22:36:32 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if(Auth::id())
+        {
+            $usertype = Auth::user()->usertype;
+
+            if($usertype == 'admin')
+            {
+                return view('admin.dashboard');
+            }
+            else if($usertype == 'user')
+            {
+                return view('dashboard');
+            }
+            else
+            {
+                return view('home');
+            }
+        }
+    }
+
+    public function homepage()
+    {
+        return view('home.homepage');
+    }
+
+    public function user()
+    {
+        $users = User::all();
+        return view('admin.user',compact('users'));
+    }
+}
